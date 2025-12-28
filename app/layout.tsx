00e4cbbd17d2e42043fb32526bcf577df5166b8b
Hugo Belém <belem.hugo@hotmail.com>
2025-12-28 21:48:04 +0100

    add: favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,28 @@ export default function RootLayout({
           name="viewport"
           content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
         ></meta>
+        <link
+          rel="icon"
+          type="image/x-ico"
+          href="../static/assets/favicon.ico"
+        ></link>
+        <link 
+          rel="apple-touch-icon" 
+          sizes="180x180" 
+          href="../static/assets/apple-touch-icon.png">
+        </link>
+        <link 
+          rel="icon" 
+          type="image/png" 
+          sizes="32x32" 
+          href="../static/assets/favicon-32x32.png">
+        </link>
+        <link 
+          rel="icon" 
+          type="image/png" 
+          sizes="16x16" 
+          href="../static/assets/favicon-16x16.png">
+        </link>
       </head>
       <body className={`${inter.className} antialiased bg-[#f7f7f5]`}>
         <section className="flex flex-col">
