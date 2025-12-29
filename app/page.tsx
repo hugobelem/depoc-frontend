1b56e73890e30d4fb8feadfadb85050bac72288c
Hugo Belém <belem.hugo@hotmail.com>
2025-12-29 02:58:03 +0100

    feat: insert dynamic content to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,69 +1,63 @@
+import clsx from "clsx";
 import { Box } from "./ui/components/box";
 import { Header, HeaderMobile } from "./ui/components/header";
-import { Request } from "./lib/api/request";
-
-  type Data = {
-    user: {
-      id: string;
-      name: string;
-      email: string;
-      username: string;
-    }
-  }
+import { TotalBalance, TotalBalanceFormatted } from "./lib/api/finance/utils";
+import { UserAPI } from "./lib/api/user/api";
 
 export default async function Home() {
-  const data = await Request<Data>("/me")
+  const user = await UserAPI();
   return (
-      <div className="list-none grid grid-cols-2 gap-3 md:grid-cols-2 lg:grid-cols-3 lg:grid-rows-3 xl:grid-cols-4 xl:grid-rows-4">
-        <Header
-          firstName={data.user.name.toUpperCase()}
-          email={data.user.email}
-          description="settings and what not"
-          href="#"
-        ></Header>
+    <div className="list-none grid grid-cols-2 gap-3 md:grid-cols-2 lg:grid-cols-3 lg:grid-rows-3 xl:grid-cols-4 xl:grid-rows-4">
+      <Header
+        firstName={user.name.toUpperCase()}
+        email={user.email}
+        description="settings and what not"
+        href="#"
+      ></Header>
 
-        <HeaderMobile
-          firstName={data.user.name.toUpperCase()}
-          email={data.user.email}
-          href="#"
-        ></HeaderMobile>
+      <HeaderMobile
+        firstName={user.name.toUpperCase()}
+        email={user.email}
+        href="#"
+      ></HeaderMobile>
 
-        <Box
-          title="POS"
-          description="start selling"
-          href="#"
-          colSpan={1}
-          colSpanMobile={2}
-          minHeight="md"
-        ></Box>
+      <Box
+        title="POS"
+        description="start selling"
+        href="#"
+        colSpan={1}
+        colSpanMobile={2}
+        minHeight="md"
+      ></Box>
 
-        <Box
-          title="R$12.590,25"
-          description="total balance"
-          href="#"
-          colSpan={1}
-          colSpanMobile={1}
-          minHeight="md"
-          className="row-span-2 lg:row-start-2"
-        ></Box>
+      <Box
+        title={TotalBalanceFormatted}
+        description="total balance"
+        href="/finance"
+        colSpan={1}
+        colSpanMobile={1}
+        minHeight="md"
+        className="row-span-2 lg:row-start-2"
+        titleClassName={clsx({ "text-orange-600": TotalBalance < 0 })}
+      ></Box>
 
-        <Box
-          title="CONTACTS"
-          description="search"
-          href="#"
-          colSpan={1}
-          colSpanMobile={1}
-          minHeight="md"
-        ></Box>
+      <Box
+        title="CONTACTS"
+        description="search"
+        href="#"
+        colSpan={1}
+        colSpanMobile={1}
+        minHeight="md"
+      ></Box>
 
-        <Box
-          title="PRODUCTS"
-          description="skus"
-          href="#"
-          colSpan={1}
-          colSpanMobile={1}
-          minHeight="md"
-        ></Box>
-      </div>
+      <Box
+        title="PRODUCTS"
+        description="skus"
+        href="#"
+        colSpan={1}
+        colSpanMobile={1}
+        minHeight="md"
+      ></Box>
+    </div>
   );
 }
