575274104bbe5810aa95e9d1c06d412873cddafa
Hugo Belém <belem.hugo@hotmail.com>
2025-12-30 16:20:22 +0100

    chore: rewrite with reduce

--- app/lib/api/finance/accounts/utils.ts
+++ app/lib/api/finance/accounts/utils.ts
@@ -3,14 +3,6 @@ export const dynamic = "force-dynamic";
 import { FinanceAPI } from "./api";
 
 export async function calculateTotalBalance() {
-  let totalBalance: number = 0;
   const results = await FinanceAPI();
-
-  if (results)
-    for (const result of results) {
-      const balance = Number(result.account.balance);
-      totalBalance += balance;
-    }
-
-  return totalBalance;
+  return results?.reduce((acc, r) => acc + Number(r.account.balance), 0) ?? 0;
 }
